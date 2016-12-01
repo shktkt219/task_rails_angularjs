3d1028cf31babfb5125f4395ee53bff4ac7f6128
shoko furushita <shktkt219@gmail.com>
2016-12-01 14:27:34 -0500

    built serializer of todolist and todo

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,7 +1,10 @@
 module Api
-   class TodoListController < ApplicationController
+   class TodoListsController < ApplicationController
      def show
        @todo_list = TodoList.find(params[:id])
+       respond_to do |f|
+         f.json { render json: @todo_list }
+       end
      end
    end
 end
