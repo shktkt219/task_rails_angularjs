f140fdaed79fbe9b1a06360f7ce1621d456826c9
shoko furushita <shktkt219@gmail.com>
2016-12-01 10:28:58 -0500

    add js file to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
+//= require angular
+//= require app
 //= require_tree .
