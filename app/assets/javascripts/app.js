98ae9ca0007b1fc6a2aedec17257acb5f622ea27
shoko furushita <shktkt219@gmail.com>
2016-12-01 10:25:34 -0500

    add app.js

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,19 @@
+var app;
+app = angular.module('taskApp', []);
+
+// CSRF(Cross-site Request Forgery)token
+app.config(function($httpProvider){
+  var authToken;
+  authToken = $("meta[name=\"csrf-token\"]")
+  .attr("content");
+  return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+});
+
+// combine AngularJS and turbolinks
+$(document).on('page:load', function(){
+  return $('[ng-app]').each(function(){
+    var module;
+    module = $(this).attr('ng-app');
+    return angular.bootstrap(this, [module]);
+  });
+});
