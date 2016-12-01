1f815776d055299193465eaec1e0af6b33f29dc9
shoko furushita <shktkt219@gmail.com>
2016-12-01 14:05:37 -0500

    add validation and relationship to models

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,7 @@
 class Todo < ActiveRecord::Base
+  belongs_to :todo_list
+
+  validates :todo_list_id, presence: true
+  validates :description, presence: true
+  validates :done, inclusion: { in: [true, false]}
 end
