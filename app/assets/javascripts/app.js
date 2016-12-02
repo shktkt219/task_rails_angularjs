61a6dbe131b5e7e9c37945b5b9699c81ddbcedb3
shoko furushita <shktkt219@gmail.com>
2016-12-01 22:27:38 -0500

    add new routes to app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,6 +9,29 @@ app.config(function($httpProvider){
   return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
 });
 
+app.config(function($statePrivider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/home');
+
+  return $statePrivider.state('home',{
+    url: '/home',
+    templateUrl: '/templates/home.html'
+  }).state('index', {
+    url: '/',
+    templateUrl: '/templates/index.html'
+  }).state('index.dashboard', {
+    url: 'dashboard',
+    templateUrl: '/templates/dashboard.html',
+    controller: 'DashboardCtrl'
+  }).state('index.about', {
+    url: 'about',
+    templateUrl: '/templates/about.html'
+  }).state('index.todolist', {
+    url: 'todo_lists/:list_id',
+    templateUrl: '/templates/todo_list.html',
+    controller: 'TodoListCtrl'
+  });
+});
+
 // AngularJS can work with turbolinks
 $(document).on('page:load', function(){
   return $('[ng-app]').each(function(){
