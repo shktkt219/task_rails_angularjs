73ff97e0988f1a811125450d9feb2df1e01614e3
shoko furushita <shktkt219@gmail.com>
2016-12-01 21:38:01 -0500

    add all, create, delete to TodoListService

--- app/assets/javascripts/services/TodoListService.js
+++ app/assets/javascripts/services/TodoListService.js
@@ -12,6 +12,12 @@ angular.module('taskApp').factory('TodoList', function($resource, $http) {
       this.errorHandler = errorHandler;
     }
 
+    TodoList.prototype.all = function(){
+      return this.service.query((function(){
+        return null;
+      }), this.errorHandler);
+    };
+
     TodoList.prototype.find = function(id, successHandler) {
       return this.service.get({
         id: id
@@ -20,6 +26,23 @@ angular.module('taskApp').factory('TodoList', function($resource, $http) {
           successHandler(list);
         }
         return list;
+      }), this.errorHandler);
+    };
+
+    TodoList.prototype.create = function(attrs){
+      new this.service({
+        todo_list: attrs
+      }).$save((function(list){
+        return attrs.id = list.id;
+      }), this.errorHandler);
+      return attrs;
+    };
+
+    TodoList.prototype["delete"] = function(list){
+      return new this.service().$delete({
+        id: list.id
+      }, (function(){
+        return null;
       }), this.errorHandler);
     };
 
