bb5580d918683022a10dd325882a723da410158b
shoko furushita <shktkt219@gmail.com>
2016-12-03 15:12:57 -0500

    edit Dashboard and TodoList Ctrl

--- app/assets/javascripts/controllers/DashboardCtrl.js
+++ app/assets/javascripts/controllers/DashboardCtrl.js
@@ -1,27 +1,29 @@
-angular.module('taskApp').controller("DashboardCtrl", function($scope, TodoList){
-  $scope.init = function(){
-    this.listsService = new TodoList(serverErrorHandler);
-    return $scope.lists = this.listsService.all();
-  };
+angular
+  .module('taskApp')
+  .controller("DashboardCtrl", function($scope, TodoList){
+    $scope.init = function(){
+      this.listsService = new TodoList(serverErrorHandler);
+      return $scope.lists = this.listsService.all();
+    };
 
-  $scope.createList = function(listName){
-    var list;
-    list = this.listsService.create({
-      name: listName
-    });
-    $scope.lists.push(list);
-    return $scope.listName = "";
-  };
+    $scope.createList = function(listName){
+      var list;
+      list = this.listsService.create({
+        name: listName
+      });
+      $scope.lists.push(list);
+      return $scope.listName = "";
+    };
 
-  $scope.deleteList = function(list, index){
-    if (confirm("Delete List?")){
-      this.listsService["delete"](list);
-      return $scope.lists.splice(index, 1);
-    }
-  };
+    $scope.deleteList = function(list, index){
+      if (confirm("Delete List?")){
+        this.listsService["delete"](list);
+        return $scope.lists.splice(index, 1);
+      }
+    };
 
-  var serverErrorHandler;
-  serverErrorHandler = function(){
-    return alert("server error");
-  };
+    var serverErrorHandler;
+    serverErrorHandler = function(){
+      return alert("server error");
+    };
 });
--- app/assets/javascripts/controllers/TodoListCtrl.js
+++ app/assets/javascripts/controllers/TodoListCtrl.js
@@ -1,48 +1,50 @@
-angular.module('taskApp').controller("TodoListCtrl", function($scope, $stateParams, TodoList, Todo) {
-  $scope.init = function() {
-    this.todoListService = new TodoList(serverErrorHandler);
-    this.todoService = new Todo($stateParams.list_id, serverErrorHandler);
-    return $scope.list = this.todoListService.find($stateParams.list_id, function(res){
-      return $scope.totalTodos = res.totalTodos;
-    });
-  };
+angular
+  .module('taskApp')
+  .controller("TodoListCtrl", function($scope, $stateParams, TodoList, Todo) {
+    $scope.init = function() {
+      this.todoListService = new TodoList(serverErrorHandler);
+      this.todoService = new Todo($stateParams.list_id, serverErrorHandler);
+      return $scope.list = this.todoListService.find($stateParams.list_id, function(res){
+        return $scope.totalTodos = res.totalTodos;
+      });
+    };
 
-  $scope.addTodo = function(todoDescription) {
-    var todo;
-    todo = this.todoService.create({
-      description: todoDescription,
-      done: false
-    });
-    $scope.list.todos.push(todo);
-    return $scope.todoDescription = "";
-  };
+    $scope.addTodo = function(todoDescription) {
+      var todo;
+      todo = this.todoService.create({
+        description: todoDescription,
+        done: false
+      });
+      $scope.list.todos.push(todo);
+      return $scope.todoDescription = "";
+    };
 
-  $scope.deleteTodo = function(todo) {
-    this.todoService["delete"](todo);
-    return $scope.list.todos.splice($scope.list.todos.indexOf(todo), 1);
-  };
+    $scope.deleteTodo = function(todo) {
+      this.todoService["delete"](todo);
+      return $scope.list.todos.splice($scope.list.todos.indexOf(todo), 1);
+    };
 
-  $scope.toggleTodo = function(todo){
-    return this.todoService.update(todo, {
-      done: todo.done
-    });
-  };
+    $scope.toggleTodo = function(todo){
+      return this.todoService.update(todo, {
+        done: todo.done
+      });
+    };
 
-  $scope.filter = {
-    done: {
-      done: true
-    },
-    remaining: {
-      done: false
-    }
-  };
+    $scope.filter = {
+      done: {
+        done: true
+      },
+      remaining: {
+        done: false
+      }
+    };
 
-  $scope.changeFilter = function(filter){
-    return $scope.currentFilter = filter;
-  };
+    $scope.changeFilter = function(filter){
+      return $scope.currentFilter = filter;
+    };
 
-  var serverErrorHandler
-  serverErrorHandler = function(){
-    alert("server error");
-  };
+    var serverErrorHandler
+    serverErrorHandler = function(){
+      alert("server error");
+    };
 });
