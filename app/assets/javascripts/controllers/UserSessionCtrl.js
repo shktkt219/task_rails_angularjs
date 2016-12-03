94e74249e31733aaebeb973be502a362f03ff857
shoko furushita <shktkt219@gmail.com>
2016-12-03 16:49:37 -0500

    add error function to sign_in

--- app/assets/javascripts/controllers/UserSessionCtrl.js
+++ app/assets/javascripts/controllers/UserSessionCtrl.js
@@ -1,4 +1,7 @@
 angular
    .module('taskApp')
    .controller('UserSessionCtrl', ['$scope', function($scope){
+     $scope.$on('auth:login-error', function(ev, reason){
+       $scope.error = reason.errors[0];
+     });
    }]);
