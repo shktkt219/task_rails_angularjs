b2699338e10457f4b02348a1db651f068978df25
shoko furushita <shktkt219@gmail.com>
2016-12-03 20:46:08 -0500

    edit user model to delete minimumlength

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,11 @@ class User < ActiveRecord::Base
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 
-  before_save -> do
-    self.uid = SecureRandom.uuid
-    skip_confirmation!
-  end
-
   has_many :todo_lists, dependent: :destroy
   has_many :todos, through: :todo_list
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true
 
 end
